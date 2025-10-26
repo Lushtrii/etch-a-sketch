3b69a8b85ce2c2b0976d0ae30c55c69cfedf9ed9
Sean Ford <seanford@protonmail.com>
2025-10-26 09:43:06 -0700

    refactor: refactor out grid cell creation

--- script.js
+++ script.js
@@ -3,14 +3,19 @@ function createGrid(numRows, numCols) {
 
   for (let i = 0; i < numRows; i++) {
     for (let j = 0; j < numCols; j++) {
-      const gridCell = document.createElement("div");
-      gridCell.classList.add("grid-cell");
-      gridCell.addEventListener("mouseover", () => {
-        gridCell.classList.add("filled");
-      });
+      const gridCell = createGridCell();
       container.appendChild(gridCell);
     }
   }
+}
+
+function createGridCell() {
+  const gridCell = document.createElement("div");
+  gridCell.classList.add("grid-cell");
+  gridCell.addEventListener("mouseover", () => {
+    gridCell.classList.add("filled");
+  });
+  return gridCell;
 }
 
 createGrid(16, 16);
