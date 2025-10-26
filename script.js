e8af1102d7182f262880e5e266cc119f3b877895
Sean Ford <seanford@protonmail.com>
2025-10-26 10:48:19 -0700

    feat: create button to reset grid and grid side length

--- script.js
+++ script.js
@@ -1,21 +1,59 @@
+const GRID_CELL_SIDE_LENGTH_PX = 25;
 function createGrid(numRows, numCols) {
   const container = document.querySelector("#grid");
+  container.style.width = `${GRID_CELL_SIDE_LENGTH_PX * numCols}px`;
+  container.style.height = `${GRID_CELL_SIDE_LENGTH_PX * numRows}px`;
   container.addEventListener("mouseover", (e) => {
     e.target.classList.add("filled");
   });
 
+  const childrenArr = [];
   for (let i = 0; i < numRows; i++) {
     for (let j = 0; j < numCols; j++) {
-      const gridCell = createGridCell();
-      container.appendChild(gridCell);
+      childrenArr.push(generateGridCell());
     }
   }
+  container.replaceChildren(...childrenArr);
 }
 
-function createGridCell() {
+function generateGridCell() {
   const gridCell = document.createElement("div");
+  gridCell.style.width = `${GRID_CELL_SIDE_LENGTH_PX}px`;
+  gridCell.style.height = `${GRID_CELL_SIDE_LENGTH_PX}px`;
   gridCell.classList.add("grid-cell");
   return gridCell;
 }
 
-createGrid(16, 16);
+function createResetButton() {
+  const MIN_GRID_SQUARES = 1;
+  const MAX_GRID_SQUARES = 100;
+  const resetGridButton = document.createElement("button");
+  resetGridButton.textContent = "Reset grid";
+
+  resetGridButton.addEventListener("click", () => {
+    const gridSideLength = +prompt(
+      "Enter the number of grid squares per side (between 1 and 100):",
+    );
+    if (
+      gridSideLength < MIN_GRID_SQUARES ||
+      gridSideLength > MAX_GRID_SQUARES
+    ) {
+      alert(
+        `ERROR: Grid side length is outside the bounds of [${MIN_GRID_SQUARES}, ${MAX_GRID_SQUARES}]`,
+      );
+    }
+    createGrid(gridSideLength, gridSideLength);
+  });
+
+  const body = document.querySelector("body");
+  const grid = document.querySelector("#grid");
+  body.insertBefore(resetGridButton, grid);
+}
+
+function setupUI() {
+  const DEFAULT_GRID_SIDE_LENGTH = 16;
+  createResetButton();
+  createGrid(DEFAULT_GRID_SIDE_LENGTH, DEFAULT_GRID_SIDE_LENGTH);
+}
+
+setupUI();
